72b2131b446ebe03904a472a777aae4bcb4a06c4
LONG <ovenzeze@qq.com>
2024-07-28 15:03:57 +0000

    chore: Update .gitignore and nuxt.config.ts, and add login page

--- app.vue
+++ app.vue
@@ -1,18 +1,11 @@
 <template>
-  <div>
-    <!-- Main content area -->
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-
-    <!-- Global components -->
-  </div>
 </template>
 
 <script setup lang="ts">
-// Any global setup can go here
 </script>
 
 <style>
-/* Add any global styles here if needed */
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
 export default defineNuxtConfig({
+  pages: true,
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', "@nuxtjs/supabase"],
+
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
   },
+
   components: [
     {
       path: '~/components',
       pathPrefix: false,
     },
   ],
+
+  compatibilityDate: '2024-07-28',
 })
