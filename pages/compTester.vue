544059a5bd04eecb5e597ded19914101fe7c8155
Clay <ovenzeze@qq.com>
2024-09-12 18:35:49 -0700

    feat: implement driver table component
    - Add DriverTable component for displaying driver information
    - Implement sorting, filtering, and pagination functionality
    - Add inline editing for certain driver fields
    - Integrate with backend API for data fetching and updates
    - Implement responsive design for mobile and desktop views

--- pages/compTester.vue
+++ pages/compTester.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="component-tester">
     <div class="function-area">
-      <USelectMenu v-model="selectedCompName" :options="componentOptions" />
+      <USelectMenu v-model="selectedCompName" :options="componentOptions" value-attribute="value"
+        label-attribute="label" />
     </div>
     <div class="component-display mt-4">
       <component :is="selectedComp.component" v-bind="selectedComp.props" />
@@ -10,12 +11,13 @@
 </template>
 
 <script setup lang="ts">
-import { USelectMenu, FixedCard } from '#components'
+import { USelectMenu, FixedCard, DriverSelector } from '#components'
 const componentOptions = [
   { value: 'FixedCard', label: 'Fixed Card', component: FixedCard, props: { headerHeight: { mobile: 40, desktop: 50 }, footerHeight: { mobile: 40, desktop: 40 }, otherElementsHeight: { mobile: 110, desktop: 40 } } },
+  { value: 'DriverSelector', label: 'Driver Selector', component: DriverSelector, props: {} },
 ]
 const selectedCompName = ref(componentOptions[0].value)
-const selectedComp = componentOptions.find(comp => comp.value === selectedCompName.value)
+const selectedComp = computed(() => componentOptions.find((comp) => comp.value === selectedCompName.value)!)
 </script>
 
 <style scoped>
